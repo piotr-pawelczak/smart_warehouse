722203faa5c29ffc8d95f8a9b1c04a5fc0c48f61
Piotr Pawełczak <pecio@student.agh.edu.pl>
2021-10-15 13:22:48 +0200

    Add Product and Location model

--- warehouse/admin.py
+++ warehouse/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Product, Location, ProductLocation
 
 # Register your models here.
+
+
+class ProductLocationInline(admin.TabularInline):
+    model = ProductLocation
+    raw_id_fields = ['product']
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    inlines = [ProductLocationInline]
+
+
+@admin.register(Location)
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    inlines = [ProductLocationInline]
+
--- warehouse/models.py
+++ warehouse/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
+
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    quantity = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+    def get_quantity(self):
+        quantity = 0
+        for q in self.locations.all():
+            quantity += q.quantity
+        self.quantity = quantity
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=200)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductLocation(models.Model):
+    product = models.ForeignKey(Product, related_name='locations', on_delete=models.CASCADE)
+    location = models.ForeignKey(Location, related_name='products', on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    def __str__(self):
+        return f"{self.id}"
