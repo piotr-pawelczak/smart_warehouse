5072399193c5891f0c13257bcc906a610fd3b2e1
Piotr Pawełczak <pecio@student.agh.edu.pl>
2021-11-22 16:36:53 +0100

    Password change

--- account/urls.py
+++ account/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from django.contrib.auth import views as auth_views
 from . import views
 
@@ -8,5 +8,8 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('profile/', views.profile, name='profile'),
-    path('register/', views.register, name='register')
+    path('register/', views.register, name='register'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(success_url=reverse_lazy('account:password_change_done')), name='password_change'),
+    path('password_change/done', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+
 ]
