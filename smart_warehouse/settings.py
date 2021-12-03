dfdd1014ad9da6fd30e708441ca2680bf91ca506
Piotr Pawełczak <pecio@student.agh.edu.pl>
2021-12-03 15:46:02 +0100

    Api app initial commit

--- smart_warehouse/settings.py
+++ smart_warehouse/settings.py
@@ -49,6 +49,8 @@ INSTALLED_APPS = [
     'widget_tweaks',
     'django_filters',
     'phonenumber_field',
+    'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
@@ -153,3 +155,11 @@ LOGIN_URL = 'account:login'
 LOGOUT_URL = 'account:logout'
 LOGIN_REDIRECT_URL = 'warehouse:home'
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
